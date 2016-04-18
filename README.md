15477612072561d4d478db69789a114a4ea408d3
pedronovaes <pedromarcelino2006@hotmail.com>
2016-04-18 18:20:31 -0300

    Setting levels specification for categorical or factor variable

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Some codes to analyse a data set containing information on flight arrival and departure details for all commercial flights within the USA, from October 1987 to April 2008.
+Some codes to analyse a data set (600,000 rows) containing information on flight arrival and departure details for all commercial flights within the USA, from October 1987 to April 2008.
 
 For this big data analysis, I used RevoScale R, an R package that provides functions for performing scalable and extremely high performance data managemente, analysis and visualization.
 
@@ -7,3 +7,7 @@ RevoScale R are particularly useful in three common situations:
 1) to analyze data sets that are too big to fit in memory and,
 2) to perform computation distributed over several cores, processors, or nodes in a cluster, or
 3) to create scalable data analysis routines that can be developed locally with smaller data sets, then deployed to large data and/or a cluster of computers.
+
+The following tasks are performed at this work:
+
+1) Convert text data to the .xdf data file format
--- convertCsvToXdf.R
+++ convertCsvToXdf.R
@@ -1,22 +1,38 @@
 # xdf (eXtensible Data Formats) is a file format designed for large files
 # this code convert csv file to xdf file
-# its the first step in the workflow
 
 # rxGetOption is used to retrieve the data directory
-# "data_directory" is the directory that stores the csv file
-dataDir <- rxGetOption("data_directory")
+# "sampleDataDir" is the directory that stores the csv file
+dataDir <- rxGetOption("sampleDataDir")
 getwd()
-inputFile <- file.path(dataDir, "file.csv")
+inputFile <- file.path(dataDir, "AirlineDemoSmall.csv")
 
 # rxImport is used to convert a text file into the .xdf data format
 # fileXdf is an object that represents the file.xdf
 fileXdf <- rxImport(inData = inputFile,
-                    outFile = "file.xdf",
+                    outFile = "ADS.xdf",
                     missingValueString = "NA",
                     stringAsFactors = TRUE,
                     overwrite = TRUE)
 
-# we can apply some standard R fuctions to get basic information about the xdf object
+# if it's necessary to convert the variable type to categorical or factor variable, the code below do this
+# setting levels specification for DayOfWeek variable
+colInfo <- list(DayOfWeek = list(type = "factor",
+                                 levels = c("Monday",
+                                            "Tuesday",
+                                            "Wednesday",
+                                            "Thursday",
+                                            "Friday",
+                                            "Saturday",
+                                            "Sunday")))
+
+fileXdf <- rxImport(inData = inputFile,
+                    outFile = "ADS.xdf",
+                    missingValueString = "NA",
+                    colInfo = colInfo,
+                    overwrite = TRUE)
+
+# applying some standard R fuctions to get basic information about the xdf object
 nrow(fileXdf)
 ncol(fileXdf)
 head(fileXdf)
